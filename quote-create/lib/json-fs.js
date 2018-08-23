39db7ebe6fa62c8af29b335fdb890ce6952ecfb6
adi <adityabhat@lazaro.in>
2018-08-23 13:14:06 +0530

    Log Archive : Change the implementation of the `add` function

--- quote-create/lib/json-fs.js
+++ quote-create/lib/json-fs.js
@@ -20,28 +20,11 @@ function add ( fileName, record ) {
 
 	return new Promise( function ( resolve, reject ) {
 
-		fs.open( fileName, "a", function ( e, fd ) {
-			if ( e ) { return reject( e ); }
-			fs.fstat( fd, function ( e, fstat ) {
-				if ( e ) { return reject( e ); }
-				let text;
-				let writePosition;
-				if ( fstat.size == 0 ) {
-					text = "[" + JSON.stringify( record ) + "]";
-					writePosition = 0;
-				}
-				else {
-					text = "," + JSON.stringify( record ) + "]";
-					writePosition = fstat.size - 1;
-				}
-				fs.write( fd, text, writePosition, function ( e ) {
-					if ( e ) { return reject( e ); }
-					fs.close( fd, function ( e ) {
-						if ( e ) { return reject( e ); }
-						resolve( record );
-					} );
-				} );
-			} );
+		let recordText = "," + JSON.stringify( record );
+		fs.appendFile( fileName, recordText, function ( e ) {
+			if ( e )
+				return reject( e );
+			return resolve( record );
 		} );
 
 	} );
