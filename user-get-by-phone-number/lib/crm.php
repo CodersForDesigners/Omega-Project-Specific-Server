52b30a7c23bad958f4469afe4c5cde926d84aa62
adi <adityabhat@lazaro.in>
2019-04-05 14:22:03 +0530

    Users : Get by Phone Number : Search for the number in other number fields too

--- user-get-by-phone-number/lib/crm.php
+++ user-get-by-phone-number/lib/crm.php
@@ -34,19 +34,82 @@ $operatorRelationMap = [
 
 /*
  * -----
+ * Criterion stringifier
+ * -----
+ */
+function getStringifiedCriterion ( $name, $relation__value ) {
+
+	if ( empty( $relation__value ) ) {
+		$criteriaString = '';
+	}
+	else if ( is_array( $relation__value ) ) {
+		global $operatorRelationMap;
+		$operator = $relation__value[ 0 ];
+		$value = $relation__value[ 1 ];
+		$criteriaString = '(' . $name . ':' . $operatorRelationMap[ $operator ] . ':' . urlencode( $value ) . ')';
+	}
+	else {
+		$value = $relation__value;
+		$criteriaString = '(' . $name . ':equals:' . urlencode( $value ) . ')';
+	}
+
+	return $criteriaString;
+
+}
+/*
+ * -----
+ * Criteria resolver
+ * -----
+ */
+function getResolvedCriteria ( $criteria ) {
+
+	$name = array_keys( $criteria )[ 0 ];
+
+	if ( in_array( $name, [ 'or', 'and' ] ) ) {
+		$operator = $name;
+		$subCriteria = $criteria[ $operator ];
+		$subCriteriaStrings = array_map( function ( $name, $value ) {
+			return getResolvedCriteria( [ $name => $value ] );
+		}, array_keys( $subCriteria ), array_values( $subCriteria ) );
+		return '(' . implode( $operator, $subCriteriaStrings ) . ')';
+	}
+	else {
+		return getStringifiedCriterion(
+			array_keys( $criteria )[ 0 ],
+			array_values( $criteria )[ 0 ]
+		);
+	}
+
+}
+
+/*
+ * -----
  * Get user by phone number
  * -----
  */
 function getUserByPhoneNumber ( $phoneNumber, $client ) {
 
 	$user = getRecordWhere( 'Leads', [
-		'Phone' => $phoneNumber,
-		'Project' => [ '^=', $client ]
+		'and' => [
+			'Project' => [ '^=', $client ],
+			'or' => [
+				'Phone' => $phoneNumber,
+				'Alt_Mobile' => $phoneNumber
+			]
+		]
 	] );
 	if ( empty( $user ) ) {
 		$user = getRecordWhere( 'Contacts', [
-			'Phone' => $phoneNumber,
-			'Project' => [ '^=', $client ]
+			'and' => [
+				'Project' => [ '^=', $client ],
+				'or' => [
+					'Phone' => $phoneNumber,
+					'Other_Phone' => $phoneNumber,
+					'Mobile' => $phoneNumber,
+					'Home_Phone' => $phoneNumber,
+					'Asst_Phone' => $phoneNumber
+				]
+			]
 		] );
 		if ( ! empty( $user ) )
 			$user[ 'isProspect' ] = true;
@@ -63,25 +126,7 @@ function getRecordWhere ( $recordType, $criteria = [ ] ) {
 	$accessToken = $authCredentials[ 'access_token' ];
 
 	$baseURL = 'https://www.zohoapis.com/crm/v2/' . $recordType . '/search';
-	$criteriaString = '';
-
-	foreach ( $criteria as $name => $relation__value ) {
-
-		if ( empty( $relation__value ) )
-			continue;
-
-		if ( is_array( $relation__value ) ) {
-			$operator = $relation__value[ 0 ];
-			$value = $relation__value[ 1 ];
-			$criteriaString .= 'and(' . $name . ':' . $operatorRelationMap[ $operator ] . ':' . urlencode( $value ) . ')';
-		}
-		else {
-			$value = $relation__value;
-			$criteriaString .= 'and(' . $name . ':equals:' . urlencode( $value ) . ')';
-		}
-
-	}
-	$criteriaString = '?criteria=(' . substr( $criteriaString, 3 ) . ')';
+	$criteriaString = '?criteria=(' . getResolvedCriteria( $criteria ) . ')';
 	$endpoint = $baseURL . $criteriaString;
 
 	$httpRequest = curl_init();
@@ -108,24 +153,7 @@ function getRecordWhere ( $recordType, $criteria = [ ] ) {
 
 	// If more than one records were found
 	if ( $body[ 'info' ][ 'count' ] > 1 ) {
-		$errorMessage = 'More than one ' . $recordType . ' found with the given criteria; ';
-		foreach ( $criteria as $name => $relation__value ) {
-
-			if ( empty( $relation__value ) )
-				continue;
-
-			if ( is_array( $relation__value ) ) {
-				$operator = $relation__value[ 0 ];
-				$value = $relation__value[ 1 ];
-				$errorMessage .= $name . ' ' . $operatorRelationMap[ $operator ] . ' ' . $value;
-			}
-			else {
-				$value = $relation__value;
-				$errorMessage .= $name . ' equals ' . $value;
-			}
-
-		}
-		$errorMessage .= '.';
+		$errorMessage = 'More than one ' . $recordType . ' found with the given criteria; ' . json_encode( $criteria ) . '.';
 		throw new \Exception( $errorMessage, 2 );
 	}
 
